e5c62521b56f51cd10695f843174711f47c9c12c
RichT <rrtrenchf@gmail.com>
2020-07-15 01:02:51 -0400

    Changed port setting for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-// var PORT = 3000;
+var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -33,6 +33,4 @@ require("./routes/htmlroutes.js")(app)
 
 
 //listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+.listen(process.env.PORT || 3000)
