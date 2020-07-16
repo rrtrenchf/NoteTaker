a4c9884140955a2ce7ac8a27422b14c320308c82
RichT <rrtrenchf@gmail.com>
2020-07-15 21:54:43 -0400

    added readme

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,3 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
 // var tableData = require("../data/tableData");
 var notes = require("../db/db.json");
@@ -16,47 +11,30 @@ const { response } = require("express");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-
+//function to export routes into server file
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-
   
   app.get("/api/notes", function(req, res) {
   
     console.log("Notedata")
     res.json(notes);
   });
-
-  
-  
-  
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 // get current notes array from file and add newNote to an array
   app.post("/api/notes", function(req, res){
       var newNote = req.body
       var allNotes = [...notes,newNote]
-      //this is in memory.
+      //this is in memory. Creates a copy of the existing file so it must be set to a vaiable
+      //map method attatches id using indexed position in the notes array, reassigned it back to the notes variable
       var notesWithIds = allNotes.map((note, index) => {
         return {...note, id: index + 1}
      })
      notes = notesWithIds
-
-    //This writes it to disk
+    //This writes it to disk and 'updates' existing file so it doesn't need to be assigned to a variable
     fs.writeFileSync("./db/db.json", JSON.stringify(notesWithIds,0,2));
     
 
-      //notes.push(newNote,notesWithIds)
+      
       console.log(allNotes)
       console.log(notesWithIds)
       console.log(newNote)
@@ -66,37 +44,10 @@ module.exports = function(app) {
     
 
   });
- 
-
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a table
-//     // req.body is available since we're using the body parsing middleware
-//     if (tableData.length < 5) {
-//       tableData.push(req.body);
-//       fs.writeFileSync("./data/tableData.json", JSON.stringify(tableData, 0, 2));
-//       res.json(true);
-//     }
-//     else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-
-//     var original = ["Ahmed"]
-//     var newOne = ["Ahmed", "Josh"]
-//   });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  // app.delete("/api/clear", function(req, res) {
-  //   // Empty out the arrays of data
-  //   notes.length = 0;
-
-  // });
   
-  
+  //req.params.id set to variable id so note can be identified for deletion
   app.delete("/api/notes/:id", function(req, res) {
+    //parseInt turns 
     const id = parseInt(req.params.id)
     for(let i=0;i<notes.length;i++){
       if (notes[i].id === id){
--- server.js
+++ server.js
@@ -2,23 +2,17 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-
-// var index = require("./public/assets/js/index")
-
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// // app.use(index)
 require("./routes/apiroutes.js")(app)
 require("./routes/htmlroutes.js")(app)
-// require("../Develop/public/index.html")(app)
-// require("../Develop/public/notes.html")(app)
+
 
  
 
